27515108021ae95753cc17e0aee5457c377a9c43
parhamr <reid@copiousinc.com>
2012-08-17 22:27:22 -0700

    Removing UsersController actions and tests that represent actions in Devise.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,31 +17,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
   def edit
     @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(params[:user], :as => :root)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,55 +14,11 @@ describe UsersController do
     end
   end
 
-  describe "GET new" do
-    it "assigns a new user as @user" do
-      get :new, {}
-      assigns(:user).should be_a_new(User)
-    end
-  end
-
   describe "GET edit" do
     it "assigns the requested user as @user" do
       user
       get :edit, {:id => user.to_param}
       assigns(:user).should eq(user)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new User" do
-        expect {
-          post :create, {:user => valid_attributes}
-        }.to change(User, :count).by(1)
-      end
-
-      it "assigns a newly created user as @user" do
-        post :create, {:user => valid_attributes}
-        assigns(:user).should be_a(User)
-        assigns(:user).should be_persisted
-      end
-
-      it "redirects to the created user" do
-        post :create, {:user => valid_attributes}
-        response.should redirect_to(User.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved user as @user" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        User.any_instance.stub(:save).and_return(false)
-        post :create, {:user => {}}
-        assigns(:user).should be_a_new(User)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        User.any_instance.stub(:save).and_return(false)
-        post :create, {:user => {}}
-        response.should render_template("new")
-      end
     end
   end
 
