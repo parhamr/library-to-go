92bcf9a164917c98afec0ab1a8d144802aac532b
parhamr <reid@copiousinc.com>
2012-10-21 14:48:32 -0700

    Address routing spec now reflects nested resource of User.

--- spec/routing/addresses_routing_spec.rb
+++ spec/routing/addresses_routing_spec.rb
@@ -3,32 +3,39 @@ require "spec_helper"
 describe AddressesController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/addresses").should route_to("addresses#index")
-    end
+    let(:member) { create(:member) }
+    let(:member_uri) { "/users/#{member.id}" }
 
-    it "routes to #new" do
-      get("/addresses/new").should route_to("addresses#new")
-    end
+    context '(as a nested resource of users)' do
 
-    it "routes to #show" do
-      get("/addresses/1").should route_to("addresses#show", :id => "1")
-    end
+      it "routes to #index" do
+        get("#{member_uri}/addresses").should route_to("addresses#index", :user_id => member.id.to_s)
+      end
 
-    it "routes to #edit" do
-      get("/addresses/1/edit").should route_to("addresses#edit", :id => "1")
-    end
+      it "routes to #new" do
+        get("#{member_uri}/addresses/new").should route_to("addresses#new", :user_id => member.id.to_s)
+      end
 
-    it "routes to #create" do
-      post("/addresses").should route_to("addresses#create")
-    end
+      it "routes to #show" do
+        get("#{member_uri}/addresses/1").should route_to("addresses#show", :id => "1", :user_id => member.id.to_s)
+      end
 
-    it "routes to #update" do
-      put("/addresses/1").should route_to("addresses#update", :id => "1")
-    end
+      it "routes to #edit" do
+        get("#{member_uri}/addresses/1/edit").should route_to("addresses#edit", :id => "1", :user_id => member.id.to_s)
+      end
 
-    it "routes to #destroy" do
-      delete("/addresses/1").should route_to("addresses#destroy", :id => "1")
+      it "routes to #create" do
+        post("#{member_uri}/addresses").should route_to("addresses#create", :user_id => member.id.to_s)
+      end
+
+      it "routes to #update" do
+        put("#{member_uri}/addresses/1").should route_to("addresses#update", :id => "1", :user_id => member.id.to_s)
+      end
+
+      it "routes to #destroy" do
+        delete("#{member_uri}/addresses/1").should route_to("addresses#destroy", :id => "1", :user_id => member.id.to_s)
+      end
+
     end
 
   end
